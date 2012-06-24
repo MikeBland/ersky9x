d6243f4889f592b9f1bdccba08e88d914905a903
michael.blandford <michael.blandford@mypostoffice.co.uk>
2012-06-24 22:50:28 +0000

    Binary released

--- src/stamp-ersky9x.h
+++ src/stamp-ersky9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
 #define DATE_STR "24.06.2012"
-#define TIME_STR "22:48:49"
-#define SUB_VERS 829-Mike
-#define SVN_VERS "ersky9x-r84"
+#define TIME_STR "22:50:55"
+#define SUB_VERS 830-Mike
+#define SVN_VERS "ersky9x-r85"
 #define MOD_VERS "REVB"
