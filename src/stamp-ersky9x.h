9ff1887db1b6ad6427ecfdaea2a63eba14f71665
michael.blandford <michael.blandford@mypostoffice.co.uk>
2012-11-22 23:19:45 +0000

    Commit the binary file from r125

--- src/stamp-ersky9x.h
+++ src/stamp-ersky9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
 #define DATE_STR "22.11.2012"
-#define TIME_STR "23:13:17"
-#define SUB_VERS 1417-Mike
-#define SVN_VERS "ersky9x-r125"
+#define TIME_STR "23:19:13"
+#define SUB_VERS 1418-Mike
+#define SVN_VERS "ersky9x-r126"
 #define MOD_VERS "REVB"
