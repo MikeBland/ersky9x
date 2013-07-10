5194801fdb052d921d4420e7ca10e07e81c37ad3
michael.blandford <michael.blandford@mypostoffice.co.uk>
2013-07-10 21:23:23 +0000

    Fix FrSky user packet count

--- src/stamp-ersky9x.h
+++ src/stamp-ersky9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
-#define DATE_STR "09.07.2013"
-#define TIME_STR "11:57:01"
-#define SUB_VERS 2066-Mike
-#define SVN_VERS "ersky9x-r176"
+#define DATE_STR "10.07.2013"
+#define TIME_STR "21:22:42"
+#define SUB_VERS 2068-Mike
+#define SVN_VERS "ersky9x-r177"
 #define MOD_VERS "REVB"
