fd7be0ecb0efa6207c51b130dc457df9a25d33ab
michael.blandford <michael.blandford@mypostoffice.co.uk>
2013-10-21 22:48:01 +0000

    Correctly commit changes

--- src/stamp-ersky9x.h
+++ src/stamp-ersky9x.h
@@ -1,6 +1,6 @@
 //Automatically generated file - do not edit
 #define DATE_STR "21.10.2013"
-#define TIME_STR "22:33:38"
-#define SUB_VERS 2337-Mike
-#define SVN_VERS "ersky9x-r183"
+#define TIME_STR "22:46:41"
+#define SUB_VERS 2341-Mike
+#define SVN_VERS "ersky9x-r185"
 #define MOD_VERS "REVB"
