ff6d5bdb4c0e438b7823c034e653f7d80d1ee3b2
michael.blandford <michael.blandford@mypostoffice.co.uk>
2014-01-07 11:25:32 +0000

    more files added

---
+++ src/bin2hex.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import sys
+
+fileout = sys.argv[1]
+print fileout
+filename = sys.argv[2]
+print filename
+
+fr = open(filename, "rb")
+fw = open(fileout, "w")
+
+
+#fw.write("const uint8_t TinyApp[] = {\n") 
+
+st = fr.read(16)
+
+while st:
+  for b in st:
+    fw.write("0x%02x," % ord(b))
+  fw.write("\n")
+  st = fr.read(16)
+
+fw.write("\n") 
+
+#fw.write("} ;\n") 
+
+
+fw.close()
+
